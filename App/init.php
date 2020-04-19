3bb2f428feb4e6dd7c9d1ddbbbf3db506950509e
lcver <crackover1092@gmail.com>
2020-04-19 13:14:00 +0700

    Initialized File

---
+++ App/init.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Configuration
+ * 
+ * @return PATH
+ * MPATH, VPATH, CPATH, LPATH
+ */
+require 'config/config.php';
+
+/**
+ * Autoload Class
+ * @return class
+ */
+require 'core/Autoload.php';
+$load = new App\Core\Autoload;
