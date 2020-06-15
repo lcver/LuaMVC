7cf14c1ee69323168b9e17898caa01dde3c4ab7e
lcver <crackover1092@gmail.com>
2020-06-15 16:35:43 +0700

    [fix] Rename Folder model to models

---
+++ App/models/ProfileModel.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Initialized Class
+ * @return Init
+ */
+use App\Core\Controller;
+
+class ProfileModel extends Controller
+{
+    /**
+     * Nama
+     * @var String
+     */
+    protected $nama;
+
+    /**
+     * Umur
+     * @var Integer
+     */
+    protected $umur;
+
+    /**
+     * Asal Sekolah
+     * @var String
+     */
+    protected $sekolah;
+
+    /**
+     * Data
+     * To return
+     * @var Object
+     */
+    public $data;
+
+    /**
+     * Set Data
+     * Catch data params
+     * 
+     * @param String
+     * Nama, Sekolah
+     * @param Integer
+     * Umur
+     */
+
+    /**
+     * Get Data
+     * Return data object
+     * 
+     * @return object
+     */
+    public function get_data()
+    {
+        return Database::table('profile')
+                            ->get();
+    }
+    
+}
