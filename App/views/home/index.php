a001161236d8e610717baf5338afb62faac5b7c3
lcver <crackover1092@gmail.com>
2020-06-16 23:43:56 +0700

    [delete] Reset Template

--- App/views/home/index.php
+++ App/views/home/index.php
@@ -1,2 +1,10 @@
-    home
-</body>
+<div class="container">
+    <div class="row">
+        <div class="card">
+            test
+        </div>
+        <div class="card">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum placeat impedit atque deleniti, aperiam labore, sunt explicabo voluptates temporibus cum aut optio, dolor nemo nihil ad ipsam voluptatem voluptatum provident.
+        </div>
+    </div>
+</div>
