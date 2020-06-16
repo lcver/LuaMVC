de73d6d12f1f12e9d9a64eed6cc5ade64070cf71
lcver <crackover1092@gmail.com>
2020-06-16 14:10:12 +0700

    [add] sweetener layout

---
+++ App/views/templates/navbar.php
@@ -0,0 +1,15 @@
+<nav>
+    <div class="nav-left">
+        <div class="brand">
+            <p>
+                <span>Lu</span>cver
+            </p>
+        </div>
+    </div>
+    <div class="nav-right">
+        <ul class="nav-menu">
+            <li class="nav-item"><a href="">Home</a></li>
+            <li class="nav-item"><a href="">About</a></li>
+        </ul>
+    </div>
+</nav>
