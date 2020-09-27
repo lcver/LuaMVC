6010d630d5a75c36521a160fc6602034ceabf5bc
lcver <crackover1092@gmail.com>
2020-09-27 07:20:36 +0700

    [fix] Home controller not found

--- App/core/App.php
+++ App/core/App.php
@@ -18,14 +18,14 @@ class App
          * 
          * @return Controller
          */
+        $this->controller = $this->controller."Controller";
+
         if(isset($url[0])){
-            if(file_exists(CPATH.$url[0].'Controller.php'))
+            if(file_exists(CPATH.$url[0].'.php'))
             {
-                $this->controller = $url[0].'Controller';
+                $this->controller = $url[0];
                 unset($url[0]);
             }
-        } else {
-            $this->controller = $this->controller.'Controller';
         }
         // var_dump($this->controller);
 
