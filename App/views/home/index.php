d0ffda64d9da3efec773ab6c7dbf4dfa9ce3f12e
lcver <crackover1092@gmail.com>
2020-11-22 21:33:04 +0700

    [test] Section in page

--- App/views/home/index.php
+++ App/views/home/index.php
@@ -1,10 +1,26 @@
-<div class="container">
-    <div class="row">
-        <div class="card">
-            test
-        </div>
-        <div class="card">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum placeat impedit atque deleniti, aperiam labore, sunt explicabo voluptates temporibus cum aut optio, dolor nemo nihil ad ipsam voluptatem voluptatum provident.
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?= $section['header'] ?>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="card">
+                test
+            </div>
+            <div class="card">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum placeat impedit atque deleniti, aperiam labore, sunt explicabo voluptates temporibus cum aut optio, dolor nemo nihil ad ipsam voluptatem voluptatum provident.
+            </div>
         </div>
     </div>
-</div>
+
+</body>
+</html>
