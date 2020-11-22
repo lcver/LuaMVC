94fac48d8609da2cc99ebd7139dfa54fc8cc071e
lcver <crackover1092@gmail.com>
2020-11-22 21:32:16 +0700

    [add] Section Controller

--- App/core/Controller.php
+++ App/core/Controller.php
@@ -6,9 +6,13 @@ class Controller
     /**
      * View Controller
      * @return Pageview
-     * @return Array $data
+     * 
+     * @param View View Page
+     * @param Array $data
      */
     public function view(String $view, $data = null){
+
+        $section['header'] = $this->section('home/header');
         require VPATH.$view.'.php';
 
     }
@@ -20,5 +24,22 @@ class Controller
     public function model(String $model){
         require MPATH.$model.'.php';
         return new $model;
+    }
+
+    /**
+     * Section Controller
+     * @return Content
+     */
+    public function section(String $view)
+    {
+        /** Validation */
+        if ( !file_exists(VPATH.$view.".php") )
+        {
+            return "File No Existing";
+        }
+
+
+        /** Geting Content */
+        return file_get_contents(VPATH.$view.".php");
     }
 }
